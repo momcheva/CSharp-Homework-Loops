2097357846833210aef8053f267cfb48da730d09
momcheva <nora.momcheva@gmail.com>
2016-06-23 12:28:06 +0300

    create P01.NumberFromOneToN

---
+++ NumberFromOneToN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.NumberFromOneToN
+{
+    class NumberFromOneToN
+    {
+        static void Main()
+        {
+            //Version 1
+            int n = int.Parse(Console.ReadLine());
+            int counter=1;
+
+            while (counter<=n)
+                {
+                    Console.Write("{0} ", counter);
+                    counter++;
+                }
+
+            Console.WriteLine();
+
+            /* Version 2
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+                {
+                    Console.Write(i +' ');
+                }
+            Console.WriteLine();
+            */
+
+        }
+    }
+}
